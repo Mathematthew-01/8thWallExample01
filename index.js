5175acac1fe1fab30c3daf50cb94874f25d1a55e
Mathematthew-01 <61673878+Mathematthew-01@users.noreply.github.com>
2020-04-15 12:09:30 +1000

    Add files via upload

--- index.js
+++ index.js
@@ -1,122 +1,147 @@
-const modelRootURL = './'                                 // Directory where 3D model lives
-const modelFile = 'tree.glb'                              // 3D model to spawn at tap
-const startScale = new BABYLON.Vector3(0.01, 0.01, 0.01)  // Initial scale value for our model
-const endScale = new BABYLON.Vector3(0.05, 0.05, 0.05)    // Ending scale value for our model
-const animationMillis = 750                               // Animate over 0.75 seconds
+// Copyright (c) 2018 8th Wall, Inc.
 
-let surface, engine, scene, camera
+// Returns a pipeline module that initializes the threejs scene when the camera feed starts, and
+// handles subsequent spawning of a glb model whenever the scene is tapped.
+const placegroundScenePipelineModule = () => {
+  const modelFile = 'tree.glb'                                 // 3D model to spawn at tap
+  const startScale = new THREE.Vector3(0.0001, 0.0001, 0.0001) // Initial scale value for our model
+  const endScale = new THREE.Vector3(0.002, 0.002, 0.002)      // Ending scale value for our model
+  const animationMillis = 750                                  // Animate over 0.75 seconds
 
-// Populates some object into an XR scene and sets the initial camera position.
-const initXrScene = ({ scene, camera }) => {
-  console.log('initXrScene')
+  const raycaster = new THREE.Raycaster()
+  const tapPosition = new THREE.Vector2()
+  const loader = new THREE.GLTFLoader()  // This comes from GLTFLoader.js.
 
-  const directionalLight = new BABYLON.DirectionalLight("DirectionalLight", new BABYLON.Vector3(0, -1, 1), scene)
-  directionalLight.intensity = 1.0
+  let surface  // Transparent surface for raycasting for object placement.
 
-  const ground = BABYLON.Mesh.CreatePlane('ground', 100, scene)
-  ground.rotation.x = Math.PI / 2
-  ground.material = new BABYLON.StandardMaterial("groundMaterial", scene)
-  ground.material.alpha = 0
-  surface = ground
+  // Populates some object into an XR scene and sets the initial camera position. The scene and
+  // camera come from xr3js, and are only available in the camera loop lifecycle onStart() or later.
+  const initXrScene = ({ scene, camera }) => {
+    console.log('initXrScene')
+    surface = new THREE.Mesh(
+      new THREE.PlaneGeometry( 100, 100, 1, 1 ),
+      new THREE.MeshBasicMaterial({
+        color: 0xffff00,
+        transparent: true,
+        opacity: 0.0,
+        side: THREE.DoubleSide
+      })
+    )
 
-  // Set the initial camera position relative to the scene we just laid out. This must be at a
-  // height greater than y=0.
-  camera.position = new BABYLON.Vector3(0,3,5)
-}
+    surface.rotateX(-Math.PI / 2)
+    surface.position.set(0, 0, 0)
+    scene.add(surface)
 
-const placeObjectTouchHandler = (e) => {
-  // console.log('placeObjectTouchHandler')
-  // Call XrController.recenter() when the canvas is tapped with two fingers. This resets the
-  // AR camera to the position specified by XrController.updateCameraProjectionMatrix() above.
-  if (e.touches.length == 2) {
-    XR8.XrController.recenter()
+    scene.add(new THREE.AmbientLight( 0x404040, 5 ))  // Add soft white light to the scene.
+
+    // Set the initial camera position relative to the scene we just laid out. This must be at a
+    // height greater than y=0.
+    camera.position.set(0, 3, 0)
   }
 
-  if (e.touches.length > 2) {
-    return
+  const animateIn = (model, pointX, pointZ, yDegrees) => {
+    console.log(`animateIn: ${pointX}, ${pointZ}, ${yDegrees}`)
+    const scale = Object.assign({}, startScale)
+
+    model.scene.rotation.set(0.0, yDegrees, 0.0)
+    model.scene.position.set(pointX, 0.0, pointZ)
+    model.scene.scale.set(scale.x, scale.y, scale.z)
+    XR8.Threejs.xrScene().scene.add(model.scene)
+
+    new TWEEN.Tween(scale)
+      .to(endScale, animationMillis)
+      .easing(TWEEN.Easing.Elastic.Out) // Use an easing function to make the animation smooth.
+      .onUpdate(() => { model.scene.scale.set(scale.x, scale.y, scale.z) })
+      .start() // Start the tween immediately.
   }
 
-  // If the canvas is tapped with one finger and hits the "surface", spawn an object.
-
-  const pickResult = scene.pick(e.touches[0].clientX, e.touches[0].clientY)
-  if (pickResult.hit && pickResult.pickedMesh == surface) {
-
-    const gltf = BABYLON.SceneLoader.LoadAssetContainer(
-      modelRootURL,
-      modelFile,
-      scene,
-      function (container) {  // onSuccess
-        const scale = Object.assign({}, startScale)
-        const yRot = Math.random() * 360
-        for (i = 0; i < container.meshes.length; i++) {
-          container.meshes[i]._position.x = pickResult.pickedPoint.x
-          container.meshes[i]._position.z = pickResult.pickedPoint.z
-          container.meshes[i]._rotation.y = yRot
-          container.meshes[i]._scaling.x = scale.x
-          container.meshes[i]._scaling.y = scale.y
-          container.meshes[i]._scaling.z = scale.z
-        }
-        // Adds all elements to the scene
-        container.addAllToScene()
-
-        new TWEEN.Tween(scale)
-        .to(endScale, animationMillis)
-        .easing(TWEEN.Easing.Elastic.Out) // Use an easing function to make the animation smooth.
-        .onUpdate(() => {
-          for (i = 0; i < container.meshes.length; i++) {
-            container.meshes[i]._scaling.x = scale.x
-            container.meshes[i]._scaling.y = scale.y
-            container.meshes[i]._scaling.z = scale.z
-          }
-        })
-        .start() // Start the tween immediately.
-      },
-      function (xhr) { //onProgress
-        console.log(`${(xhr.loaded / xhr.total * 100 )}% loaded`)
-      },
-      function (error) { //onError
-        console.log('Error loading model')
-      },
+  // Load the glb model at the requested point on the surface.
+  const placeObject = (pointX, pointZ) => {
+    console.log(`placing at ${pointX}, ${pointZ}`)
+    loader.load(
+      modelFile,                                                              // resource URL.
+      (gltf) => { animateIn(gltf, pointX, pointZ, Math.random() * 360) },     // loaded handler.
+      (xhr) => {console.log(`${(xhr.loaded / xhr.total * 100 )}% loaded`)},   // progress handler.
+      (error) => {console.log('An error happened')}                           // error handler.
     )
   }
-}
 
-const startScene = () => {
-  const canvas = document.getElementById('renderCanvas')
+  const placeObjectTouchHandler = (e) => {
+    console.log('placeObjectTouchHandler')
+    // Call XrController.recenter() when the canvas is tapped with two fingers. This resets the
+    // AR camera to the position specified by XrController.updateCameraProjectionMatrix() above.
+    if (e.touches.length == 2) {
+      XR8.XrController.recenter()
+    }
 
-  engine = new BABYLON.Engine(canvas, true, { stencil: true, preserveDrawingBuffer: true }, true)
-  engine.enableOfflineSupport = false
+    if (e.touches.length > 2) {
+      return
+    }
 
-  scene = new BABYLON.Scene(engine)
-  camera = new BABYLON.FreeCamera('camera', new BABYLON.Vector3(0, 0, 0), scene)
+    // If the canvas is tapped with one finger and hits the "surface", spawn an object.
+    const {scene, camera} = XR8.Threejs.xrScene()
 
-  initXrScene({ scene, camera }) // Add objects to the scene and set starting camera position.
+    // calculate tap position in normalized device coordinates (-1 to +1) for both components.
+    tapPosition.x = (e.touches[0].clientX / window.innerWidth) * 2 - 1
+    tapPosition.y = - (e.touches[0].clientY / window.innerHeight) * 2 + 1
 
-  // Connect the camera to the XR engine and show camera feed
-  camera.addBehavior(XR8.Babylonjs.xrCameraBehavior(), true)
+    // Update the picking ray with the camera and tap position.
+    raycaster.setFromCamera(tapPosition, camera)
 
-  canvas.addEventListener('touchstart', placeObjectTouchHandler, true)  // Add touch listener.
+    // Raycast against the "surface" object.
+    const intersects = raycaster.intersectObject(surface)
 
-  engine.runRenderLoop(() => {
-    // Enable TWEEN animations.
-    TWEEN.update(performance.now())
-    scene.render()
-  })
+    if (intersects.length == 1 && intersects[0].object == surface) {
+      placeObject(intersects[0].point.x, intersects[0].point.z)
+    }
+  }
 
-  window.addEventListener('resize', () => {
-    engine.resize()
-  })
+  return {
+    // Pipeline modules need a name. It can be whatever you want but must be unique within your app.
+    name: 'placeground',
+
+    // onStart is called once when the camera feed begins. In this case, we need to wait for the
+    // XR8.Threejs scene to be ready before we can access it to add content. It was created in
+    // XR8.Threejs.pipelineModule()'s onStart method.
+    onStart: ({canvas, canvasWidth, canvasHeight}) => {
+      const {scene, camera} = XR8.Threejs.xrScene()  // Get the 3js sceen from xr3js.
+
+      initXrScene({ scene, camera }) // Add objects to the scene and set starting camera position.
+
+      canvas.addEventListener('touchstart', placeObjectTouchHandler, true)  // Add touch listener.
+
+      // Enable TWEEN animations.
+      animate()
+      function animate(time) {
+        requestAnimationFrame(animate)
+        TWEEN.update(time)
+      }
+
+      // Sync the xr controller's 6DoF position and camera paremeters with our scene.
+      XR8.XrController.updateCameraProjectionMatrix({
+        origin: camera.position,
+        facing: camera.quaternion,
+      })
+    },
+  }
 }
 
 const onxrloaded = () => {
   XR8.addCameraPipelineModules([  // Add camera pipeline modules.
+    // Existing pipeline modules.
+    XR8.GlTextureRenderer.pipelineModule(),      // Draws the camera feed.
+    XR8.Threejs.pipelineModule(),                // Creates a ThreeJS AR Scene.
+    XR8.XrController.pipelineModule(),           // Enables SLAM tracking.
     XRExtras.AlmostThere.pipelineModule(),       // Detects unsupported browsers and gives hints.
     XRExtras.FullWindowCanvas.pipelineModule(),  // Modifies the canvas to fill the window.
     XRExtras.Loading.pipelineModule(),           // Manages the loading screen on startup.
     XRExtras.RuntimeError.pipelineModule(),      // Shows an error image on runtime error.
+    // Custom pipeline modules.
+    placegroundScenePipelineModule(),
   ])
 
-  startScene()
+  // Open the camera and start running the camera run loop.
+  XR8.run({canvas: document.getElementById('camerafeed')})
 }
 
 // Show loading screen before the full XR library has been loaded.
